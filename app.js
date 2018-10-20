44ead5a0da56dc245e3ad811e4c427b0fdca66a9
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2018-10-20 18:46:28 -0400

    Treating routes as controllers for MVC like implementation.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+var indexRouter = require('./controllers/index')
+var usersRouter = require('./controllers/users')
 
 var app = express()
 
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+// controllers
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
