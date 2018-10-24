1bb889b8c822d92a968707a2572aa347b9ee7b02
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2018-10-23 22:07:06 -0400

    Switched to MemoryStore for sessions.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const uuidv4 = require('uuid/v4')
 const session = require('express-session')
-const filestore = require('session-file-store')(session)
+const MemoryStore = require('memorystore')(session)
 
 const indexRouter = require('./controllers/index')
 const usersRouter = require('./controllers/users')
@@ -30,7 +30,9 @@ app.use(session({
 	genid: function(req) {
 		return uuidv4()
 	},
-	store: new filestore()
+	store: new MemoryStore({
+		checkPeriod: 86400000 // prune expired entries every 24h
+	})
 }))
 
 // controllers
