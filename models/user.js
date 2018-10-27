1981f63b53b4243fb09542041abc397ab694f28b
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2018-10-27 16:41:11 -0400

    Fixed whitespace issues on main code.

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    userName: DataTypes.STRING,
-    password: DataTypes.STRING,
-    realName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+	const User = sequelize.define('User', {
+		userName: DataTypes.STRING,
+		password: DataTypes.STRING,
+		realName: DataTypes.STRING,
+		email: DataTypes.STRING
+	}, {});
+	User.associate = function(models) {
+		// associations can be defined here
+	};
+	return User;
 };
--- seeders/20181025204259-test-user.js
+++ seeders/20181025204259-test-user.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Users', [{
-        userName: 'joe',
-        password: 'password',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }], {});
-  },
+	up: (queryInterface, Sequelize) => {
+			return queryInterface.bulkInsert('Users', [{
+				userName: 'joe',
+				password: 'password',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}], {});
+	},
 
-  down: (queryInterface, Sequelize) => {
-      return queryInterface.bulkDelete('Users', null, {});
-  }
+	down: (queryInterface, Sequelize) => {
+			return queryInterface.bulkDelete('Users', null, {});
+	}
 };
